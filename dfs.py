16b741c06a2bfd98ce6202b85f7f137da707430e
BharatJadhav2006 <bj7332108@gmail.com>
2025-11-09 17:59:51 +0530

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,25 @@
+def dfs(graph, start, visited):
+    print(start, end=" ")
+    visited[start] = True
+    for i in range(len(graph[start])):
+        if graph[start][i] == 1 and not visited[i]:
+            dfs(graph, i, visited)
+
+n = int(input("Enter number of locations: "))
+locations = []
+for i in range(n):
+    loc = input(f"Enter location name {i+1}: ")
+    locations.append(loc)
+
+graph = []
+print("Enter adjacency matrix:")
+for i in range(n):
+    row = list(map(int, input().split()))
+    graph.append(row)
+
+start_loc = input("Enter starting location: ")
+start_index = locations.index(start_loc)
+visited = [False]*n
+
+print("DFS Traversal:")
+dfs(graph, start_index, visited)
